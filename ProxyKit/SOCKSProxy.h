c84213ed7987a69738279326d60d3e05ac5b9448
Chris Ballinger <chrisballinger@gmail.com>
2014-04-27 19:13:14 -0700

    Add SOCKS server stats

--- ProxyKit/SOCKSProxy.h
+++ ProxyKit/SOCKSProxy.h
@@ -10,11 +10,45 @@
 #import "GCDAsyncSocket.h"
 #import "SOCKSProxySocket.h"
 
+@class SOCKSProxy;
+
+@protocol SOCKSProxyDelegate <NSObject>
+- (void) socksProxy:(SOCKSProxy*)socksProxy clientDidConnect:(SOCKSProxySocket*)clientSocket;
+- (void) socksProxy:(SOCKSProxy*)socksProxy clientDidDisconnect:(SOCKSProxySocket*)clientSocket;
+@end
+
+/**
+ *  SOCKS proxy server implementation.
+ */
 @interface SOCKSProxy : NSObject <GCDAsyncSocketDelegate, SOCKSProxySocketDelegate>
 
 @property (nonatomic, readonly) uint16_t listeningPort;
+@property (nonatomic, weak) id<SOCKSProxyDelegate> delegate;
+@property (nonatomic) dispatch_queue_t callbackQueue;
+@property (nonatomic, readonly) NSUInteger connectionCount;
+
+/**
+ *  Total number of bytes written during lifetime of SOCKSProxy.
+ *  @see resetNetworkStatistics
+ */
+@property (nonatomic, readonly) NSUInteger totalBytesWritten;
+
+/**
+ *  Total number of bytes read during lifetime of SOCKSProxy.
+ *  @see resetNetworkStatistics
+ */
+@property (nonatomic, readonly) NSUInteger totalBytesRead;
+
+/**
+ *  Sets `totalBytesWritten` and `totalBytesRead` to 0.
+ *  @see totalBytesWritten
+ *  @see totalBytesRead
+ */
+- (void) resetNetworkStatistics;
+
 
 - (void) startProxy; // defaults to port 9050
 - (void) startProxyOnPort:(uint16_t)port;
+- (void) disconnect;
 
 @end
